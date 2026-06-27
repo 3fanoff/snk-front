3dc98dd24f4834a6f21cc7512e7ac2b6d0964acc
3fanoff <3fanoff@mail.ru>
2026-06-27 14:13:14 +0300

    pages-1: format & some fixes

--- src/js/helpers/ifeven.js
+++ src/js/helpers/ifeven.js
@@ -1,5 +1,5 @@
 module.exports = function (conditional, options) {
-    if (conditional%2 === 0) {
+    if (conditional % 2 === 0) {
         return options.fn(this);
     } else {
         return options.inverse(this);
--- src/js/helpers/tag.js
+++ src/js/helpers/tag.js
@@ -2,7 +2,7 @@ const Handlebars = require('handlebars');
 
 module.exports = function (tagName, className, options) {
     const content = options.fn(this);
-    const classAttr = className ? (' class="' + className + '"') : '';
+    const classAttr = className ? ' class="' + className + '"' : '';
     const html = '<' + tagName + ' ' + classAttr + '>' + content + '</' + tagName + '>';
 
     return new Handlebars.SafeString(html);
